5de648fdabedb93d4f299110fb88253e8250bd73
liuqingjun <575666890@qq.com>
2018-02-06 15:43:14 +0800

    派遣-刘庆君 2018-2-6

--- center/src/main/java/com/godfkc/center/repository/OrderRepository.java
+++ center/src/main/java/com/godfkc/center/repository/OrderRepository.java
@@ -1,11 +1,18 @@
 package com.godfkc.center.repository;
 
 import com.godfkc.center.entity.Order;
+import com.sun.xml.internal.bind.v2.runtime.reflect.Lister;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @version 1.0
@@ -21,7 +28,20 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query(" select o from Order o where o.status<>0 and o.company.id = :companyId and o.user.id = :userId")
     List<Order> findByCompanyIdAndUserId(@Param("companyId") Long companyId,@Param("userId") Long userId);
 
+    //通过状态（status）查询派遣订单表(order) lqj add 2018-2-1
+    //@Query(value = "SELECT cs_order.city,cs_order.name from cs_order WHERE cs_order.status=?1 AND cs_order.id=1 ", nativeQuery = true)
+    Page<Order> findOrdersByType(Integer type,Pageable pageable);
+
+
+    //测试
+    List<Order> findAllByStatus(Integer status);
+
+
+
+
+
     //根据id查询预约信息
     @Query(" select o from Order o where o.id = ?1 and o.status<>0")
     Order findOrderById(Long id);
+
 }
